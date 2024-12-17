65a401fe75b09ef39a80e25884ef5df06c6df14e
Matthew Lee <matthewjameslee@hotmail.co.uk>
2024-12-17 09:58:41 +0000

    task3-viewing-comments

--- src/pages/ReadArticle.jsx
+++ src/pages/ReadArticle.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import CommentCard from '../components/CommentCard'
 import { useEffect, useState } from 'react'
-import { getArticlesById } from '../apis'
+import { getArticlesById, getCommentsByArticleId } from '../apis'
 
 
 const ReadArticle = () => {
@@ -11,12 +12,29 @@ const ReadArticle = () => {
         textAlign: "center"
     }
     const [article, setArticle] = useState(null)
+    const [comments, setComments] = useState(null)
+    const [showComments, setShowComments] = useState(false)
+    const [buttonText, setButtonText] = useState("Show Comments")
     const {articleId} = useParams()
+
+    function changeShowComments(e) {
+        e.preventDefault()
+        setShowComments(!showComments)
+        if (buttonText === "Show Comments") {
+            setButtonText("Hide Comments")
+        }
+        else {
+            setButtonText("Show Comments")
+        }
+    }
     useEffect(()=> {
         if (Number.isInteger(Number(articleId))) {
             getArticlesById(Number(articleId)).then((response)=> {
-                console.log(response)
                 setArticle(response)
+            })
+
+            getCommentsByArticleId(Number(articleId)).then((response)=> {
+                setComments(response)
             })
         }
         else {
@@ -30,16 +48,20 @@ const ReadArticle = () => {
 
     return (
         <div style={margins}>
-            <p>Date Published: {article.created_at.split("T")[0]}</p>
-            <h4>Topic: {article.topic}</h4>
+            <h3>Date Published: {article.created_at.split("T")[0]}</h3>
+            <h3>Topic: {article.topic}</h3>
             <h2>{article.title}</h2>
-            <h3>By {article.author}</h3>
+            <h2>By {article.author}</h2>
             <p>{<i className="fa-solid fa-thumbs-up" id="thumbs"></i>}{article.votes}</p>
             <img src={article.article_img_url}/>
             <p>{article.body}</p>
             <p>Comments: {article.comment_count}</p>
+            <button onClick={changeShowComments}>{buttonText}</button>
+            {showComments && comments ? (comments.map((comment)=> {
+                return <CommentCard key={comment.comment_id} articleComment={comment}/>
+            })): null}
         </div>
     )
-}
+} 
 
 export default ReadArticle
